377883df4fd135c90fc4ada020c9a21389d4ac83
DaiJie <daijie@php.net>
2024-11-02 19:36:47 +0800

    feat: support flag --full

--- bin.ts
+++ bin.ts
@@ -6,6 +6,32 @@ import queryForDeepseek from "./provider/deepseek"
 import applyDiff from "./apply-diff"
 import removeRedundantDiff from "./remove-redundant-diff"
 const { default: cfg } = await import(process.cwd() + '/chat-i18n.toml')
+import { parseArgs } from "util"
+
+const { values } = parseArgs({
+  args: Bun.argv,
+  options: {
+    full: {
+      type: 'boolean',
+      default: false,
+    },
+    help: {
+        type: 'boolean',
+        alias: 'h',
+        default: false,
+    }
+  },
+  strict: true,
+  allowPositionals: true,
+})
+
+if (values.help) {
+    console.log(`Usage: ${Bun.argv[0]} [options]`)
+    console.log(`Options:`)
+    console.log(`  --full    use full translation (default: false)`)
+    console.log(`  --help    Show this help message`)
+    process.exit(0)
+}
 
 const localeDir = './' + cfg.dir
 const localeFiles = localeDir + "/*.json"
@@ -16,13 +42,34 @@ if (!glob.match(sourceFile)) {
     console.error("Source file not found");
     process.exit(1);
 }
+
+if (values.full) {
+    console.log("Using full translation")
+    const source = await Bun.file(sourceFile).json()
+    for await (const localeFile of glob.scan()) {
+        const targetLocale = basename(localeFile, '.json')
+        if (targetLocale === cfg.source) {
+            continue; 
+        }
+        console.log(`Updating ${localeFile}`)
+        const translated = await queryForDeepseek({
+            translateDiffs: source,
+            locale: targetLocale,
+            key: cfg.deekseek.api_key
+        })
+        const file = Bun.file(localeDir + '/' + targetLocale + '.json')
+        await Bun.write(file, JSON.stringify(translated, null, 4))
+    }
+    process.exit(0)
+}
+
 const diffs = await getDiff(sourceFile)
 const patchToTranslate = removeRedundantDiff(diffs)
 
 for await (const localeFile of glob.scan()) {
     const targetLocale = basename(localeFile, '.json')
     if (targetLocale === cfg.source) {
-        continue;
+        continue; 
     }
     console.log(`Updating ${localeFile}`)
     const translated = await queryForDeepseek({
