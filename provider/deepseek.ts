ebfec48a060ee852ff806f2c17f66116595d099a
戴劼 <daijie@php.net>
2025-02-07 19:56:26 +0800

    feat: support setting a model for deepseek

--- provider/deepseek.ts
+++ provider/deepseek.ts
@@ -3,18 +3,32 @@ import removeMarkdownWrapper from "../remove-markdown-wrapper"
 export default async function deepseek({ 
     translateDiffs,
     locale,
-    key
+    key,
+    model = 'deepseek-reasoner'
 }: {
     translateDiffs: Record<string, any>;
     locale: string;
     key: string;
+    model?: 'deepseek-reasoner' | 'deepseek-chat';
 }) {
     const text = `You have rich software internationalization experience, you are good at using local and native languages to translate locale files.
     Rewrite this text in a more natural and fluent way, ensuring it sounds polished and natural, avoiding a literal translation.
     Translate this into ${locale}, but focus on idiomatic and smooth phrasing rather than a direct word-for-word translation.
     Accdording to context, make this text sound more polished and native-like by rephrasing it with a focus on clarity and fluency.
-    Output json:\n${JSON.stringify(translateDiffs)}`
+    Only Output json, DO NOT ADD EXTRA TEXT:\n${JSON.stringify(translateDiffs)}`
 
+    const config = {
+        "messages": [
+            {
+                "content": text,
+                "role": "user"
+            }
+        ],
+        "model": model,
+        "response_format": model === 'deepseek-chat' ? {
+            "type": "json_object"
+        } : undefined
+    }
     const res = await fetch('https://api.deepseek.com/chat/completions', {
         method: 'POST',
         headers: {
@@ -22,20 +36,14 @@ export default async function deepseek({
             'Authorization': `Bearer ${key}`,
             'Accept': 'application/json'
         },
-        body: JSON.stringify({
-            "messages": [
-                {
-                    "content": text,
-                    "role": "user"
-                }
-            ],
-            "model": "deepseek-chat",
-            "response_format": {
-                "type": "json_object"
-            },
-        })
+        body: JSON.stringify(config)
     })
-    const { choices } = await res.json()
+    const result = await res.json()
+    if (res.status !== 200) {
+        console.error(result)
+        throw new Error(`Deepseek API error: ${res.status} ${res.statusText}`)
+    }
+    const { choices } = result
     console.log(choices)
     return removeMarkdownWrapper(choices?.[0]?.message?.content) || null
 }
